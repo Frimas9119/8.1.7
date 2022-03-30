8ef736649170f8c24decc453531be0a0b2eb615d
Frimas9119 <90787948+Frimas9119@users.noreply.github.com>
2022-03-30 16:20:21 +0300

    Add files via upload

---
+++ 8.1.7/8.1.7.cpp
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+using namespace std;
+
+int Count(const string str, char* s)
+{
+	int z = 0;
+	for (int i = 0; i < 5;i++) {
+		for (int j = 0;j < 102;j++) {
+			if (s[i] == str[j]) {
+				z++;
+				if (z == 5) {
+					return z;
+				}
+				break;
+			}
+		}
+	}
+	return z;
+}
+
+int main()
+{
+	string str;
+	char s[] = "while";
+	cout << "Enter string:" << endl;
+	getline(cin, str);
+	cout << "there are " << Count(str, s) << "/5 " << "letters in the word 'while' " << endl;
+	return 0;
+}
